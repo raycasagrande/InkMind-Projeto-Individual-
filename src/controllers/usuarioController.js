0fcc829fe660865354b7b03029b672ed369244aa
user.name <rayssa.souza@sptech.school>
2025-05-20 23:57:49 -0300

    Adicionando a nova coluna de BD nos arquivos

--- src/controllers/usuarioController.js
+++ src/controllers/usuarioController.js
@@ -44,20 +44,23 @@ function buscar(req, res) {
 function cadastrar(req, res) {
     // Crie uma variável que vá recuperar os valores do arquivo cadastro.html
     var nome = req.body.nomeServer;
+    var nomeUsuario = req.body.nomeUsuarioServer;
     var email = req.body.emailServer;
     var senha = req.body.senhaServer;
 
     // Faça as validações dos valores
     if (nome == undefined) {
         res.status(400).send("Seu nome está undefined!");
-    } else if (email == undefined) {
+    } if (nomeUsuario == undefined) {
+        res.status(400).send("Seu nome de usuário está undefined!");
+    }else if (email == undefined) {
         res.status(400).send("Seu email está undefined!");
     } else if (senha == undefined) {
         res.status(400).send("Sua senha está undefined!");
     } else {
 
         // Passe os valores como parâmetro e vá para o arquivo usuarioModel.js
-        usuarioModel.cadastrar(nome, email, senha)
+        usuarioModel.cadastrar(nome, nomeUsuario, email, senha)
             .then(
                 function (resultado) {
                     res.json(resultado);
